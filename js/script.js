55e2a5d02ebac6b0b0246f052552345ce68d7146
olgadeth <olgu374@gmail.com>
2024-11-21 19:02:49 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
+// Smooth Scrolling for Arrow
+document.querySelector('.scroll-arrow').addEventListener('click', function (e) {
+  e.preventDefault();
+  const targetSection = document.querySelector(this.getAttribute('href'));
+  targetSection.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+  });
+});
 
